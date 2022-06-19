b94bfb1a1d356e6cf11d511269ea72c2c838eff1
nvnamsss <nvnam.c@gmail.com>
2022-06-19 14:38:23 +0700

    update my work

---
+++ card.md
@@ -0,0 +1,3 @@
+Hi 64gb, sau tất cả thì bạn cũng đã bước qua tuổi 24 với những điều mới mẻ đang chờ bạn. Có vui có buồn có tốt có xấu but everything gonna be daijoubu. Hi vọng trong thời gian tới bạn muốn dũng cảm có dũng cảm, muốn ấm áp có ấm áp, muốn riêng tư có riêng tư, muốn ngủ an có ngủ an. Bạn không cô đơn a. Cố lênnnnnnnn
+
+-- Thẻ nhớ --
--- src/pages/Wishes.tsx
+++ src/pages/Wishes.tsx
@@ -7,7 +7,7 @@ import Timeline from "../components/Timeline";
 import Slider from 'react-touch-drag-slider'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-
+import WishlistToBe from '../images/Wishlisttobe.png';
 // here we are importing some images 
 // but the Slider children can be an array of any element nodes, or your own components
 
@@ -17,51 +17,58 @@ function Wishes(): ReactElement {
     let [dislikeClicked, setDislikeClicked] = useState(true)
     let [like, setLike] = useState(0)
     const [sidebarActive, setSidebarActive] = useState(false);
-
+    const width = 800;
+    const height = 600; 
     return (
         <>
-            <Title value="FilmHot - AdFree Movie / Anime Watching Website" />
-            <div className="flex sm:hidden justify-between px-[4vw] mt-6">
-                <Link to="/" className="flex items-center gap-2">
-                    <img className="w-8 h-8" src="/icon.png" alt="" />
-                    <span className="text-xl font-medium">FilmHot</span>
-                </Link>
-
-                <button onClick={() => setSidebarActive(!sidebarActive)}>
-                    <i className="fas fa-bars text-2xl"></i>
-                </button>
-            </div>
-            <div className="flex">
-                <SideBar
-                    sidebarActive={sidebarActive}
-                    setSidebarActive={setSidebarActive}
-                />
-            </div>
-            <div className="border border-gray-50 rounded-xl px-48 py-20">
-                <header>
-                    <CardWithImage></CardWithImage>
-                    <CardWithImage></CardWithImage>
+            <Title value="Happy birthday ddanthanhh" />
+            <div className="flex items-stretch">
+                {/* <div>
                     <Timeline></Timeline>
 
-                </header>
+                </div> */}
+                <div className="flex">
+                    <img src={WishlistToBe} width={width} height={height} alt=""/>
+
+                </div>
+                <div className="flex">
+                    <a>
+                        <p>Hi mom</p>
+                    </a>
+                    <a>
+                        <img className="inline" src="assets/Happy birthday.png " width={width} height={height} alt=""/>
+                    </a>
+                </div>
+                <div className="flex">
+                    <img src="assets/Wishlist to danthanh.png " width={width} height={height} alt=""/>
+
+                </div>
+                <div className="flex">
+                    <img src="assets/Digital card.png " width={width} height={height} alt=""/>
+
+                </div>
+                {/* <header>
+
+                </header> */}
+
 
             </div>
-            <div className="flex-grow px-[4vw] md:px-8 pb-8 pt-0 overflow-hidden flex flex-col items-stretch">
-                    <Carousel>
-                        <div>
-                            <img src="assets/d1.jpg" />
-                            <p className="legend">Legend 1</p>
-                        </div>
-                        <div>
-                            <img src="assets/d2.jpg" />
-                            <p className="legend">Legend 2</p>
-                        </div>
-                        <div>
-                            <img src="assets/d3.jpg" />
-                            <p className="legend">Legend 3</p>
-                        </div>
-                    </Carousel>
-                </div>
+            {/* <div className="flex-grow px-[4vw] md:px-8 pb-8 pt-0 overflow-hidden flex flex-col items-stretch">
+                <Carousel>
+                    <div>
+                        <img src="assets/d1.jpg" />
+                        <p className="legend">Legend 1</p>
+                    </div>
+                    <div>
+                        <img src="assets/d2.jpg" />
+                        <p className="legend">Legend 2</p>
+                    </div>
+                    <div>
+                        <img src="assets/d3.jpg" />
+                        <p className="legend">Legend 3</p>
+                    </div>
+                </Carousel>
+            </div> */}
         </>
 
     )
