fdaa0ece4ebb3a09c44dbfde6311ebcadcb40523
root <root@DESKTOP-0UPII6V.localdomain>
2022-09-06 22:54:35 +0700

    Replaced JsonView by Timeline, Now Load more button will be disappeared when hitting last page

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -7,7 +7,7 @@ export interface TimelineData {
 
 export interface TimelineItem {
     title: string,
-    created_at: string,
+    occurred_at: string,
     content: string,
     tags: string[],
 }
@@ -23,18 +23,17 @@ class Timeline extends React.Component<any, any, TimelineData>  {
                 <div className='js-ajax-root mt-md-7 mb-9 mb-md-12'>
                     <div className='container-xl mx-auto p-responsive-blog js-ajax-landing'>
                         {this.props.items.map((item: TimelineItem) => {
-                            console.log("hi mom");
                             return (
                                 <article className="d-flex gutter-spacious flex-wrap position-relative timeline js-ajax-fetchable post-65764 changelog type-changelog status-publish hentry changelog-label-dependency-graph changelog-label-security-and-compliance changelog-label-supply-chain">
-                                    <div className="pb-4 pt-5 pt-md-7 col-md-5-limit position-relative">
+                                    <div className="pb-4 pt-5 pt-md-7 position-relative">
                                         <div className="changelog-single-details position-sticky top-7">
                                             <svg className='octicon octicon-commit position-absolute left-0 color-bg-default color-fg-muted' xmlns='xmlns="http://www.w3.org/2000/svg' viewBox='0 0 24 24' width={28} height={28}>
                                                 <path fillRule='evenodd' d='M15.5 11.75a3.5 3.5 0 11-7 0 3.5 3.5 0 017 0zm1.444-.75a5.001 5.001 0 00-9.888 0H2.75a.75.75 0 100 1.5h4.306a5.001 5.001 0 009.888 0h4.306a.75.75 0 100-1.5h-4.306z' fill='currentColor'></path>
                                             </svg>
-                                            <h2 className="h5-mktg">
+                                            <h2 className="h5-mktg font-adddington-medium">
                                                 <a className='Link--primary'>{item.title}</a>
                                             </h2>
-                                            <time className='d-block f5-mktg text-medium color-fg-muted mt-14px'>{item.created_at}</time>
+                                            <time className='d-block f5-mktg text-medium color-fg-muted mt-14px'>{item.occurred_at}</time>
                                             <ul className='d-inline-block list-style-none post-hero__categories mt-2-l'>
                                                 {
                                                     
@@ -53,10 +52,10 @@ class Timeline extends React.Component<any, any, TimelineData>  {
                                     </div>
 
 
-                                    <div className='col-md-7'>
-                                        <div className='pb-5 pt-0 py-md-7 changelog-single-content-wrap border-bottom'>
+                                    <div className=''>
+                                        <div className='pb-5 pt-0 py-md-5 changelog-single-content-wrap border-bottom'>
                                             <div className='post__content changelog-single-content js-show-all-target'>
-                                                <p>{item.content}</p>
+                                                <p className='font-adddington-medium'>{item.content}</p>
                                             </div>
 
                                         </div>
--- src/pages/Remembrance.tsx
+++ src/pages/Remembrance.tsx
@@ -8,12 +8,12 @@ import { getEvent, listEvent } from "../services/Event";
 
 function Remembrance(): ReactElement {
     const [data, setData] = useState([])
-    const [page, setPage] = useState(0);
+    const [page, setPage] = useState(1);
     const [keyword, setKeyword] = useState("");
-    const [total, setTotal] = useState(0);
+    const [lastPage, setLastPage] = useState(false);
     const timeoutRef = useRef<any>(null);
     
-    let page_size = 20;
+    let page_size = 5;
     async function call() {
         let res = await listEvent({
             page: page,
@@ -33,36 +33,36 @@ function Remembrance(): ReactElement {
             });
         }
         
-        setTotal(res.meta.total);
+        console.log(res.data);
+        
+        setLastPage(page* page_size > res.meta.total);
         return result;
     }
 
     async function loadmore() {
-        if (total != -1 && page_size * page >= total) {
-            return;
-        }
         let result = await call();
         setData([...data, ...result]);
     }
 
     async function reset() {
-        console.log("reset");
-        setPage(0);
+        setPage(1);
         let result = await call();
         setData(result);
     }
 
     async function init() {
+        setPage(1);
         let result = await call();
         setData(result);
     }
+    
     useEffect(() => {
-        console.log("init")
         init();
     }, []);
 
     useEffect(() => {
-        console.log("load more")
+        console.log(page);
+        
         loadmore();
     }, [page]);
 
@@ -76,23 +76,19 @@ function Remembrance(): ReactElement {
         <>
             <Header></Header>
             <TextField
+                className="auth-form-body"
+                size="small"
                 label="Keyword"
                 value={keyword}
                 onChange={(e) => setKeyword(e.target.value)}
-                margin="normal"
             />
-            <JsonView data={data}></JsonView>
-            {/* <div className="archive post-type-archive post-type-archive-changelog font-mktg hfeed no-sidebar hero-image">
-                <h1 className="gutter-spacious font-modern-computer">- Ở đây chưa có giao diện</h1>
-                <h1 className="gutter-spacious font-modern-computer">- Đây là thứ sẽ hỗ trợ thẻ nhớ giúp đan thanh lục tung kí ức mỗi khi thẻ nhớ temporarily unavailable</h1>
-                <h1 className="gutter-spacious font-modern-computer">- Sẽ được thiết kế để đan thanh có thể dễ dàng tìm kiếm chỉ với vài từ khóa ít ỏi</h1>
-                <h1 className="gutter-spacious font-modern-computer">- Đang thi công...</h1>
-            </div>
-            <div className="archive post-type-archive post-type-archive-changelog font-mktg hfeed no-sidebar">
-                <Timeline items={TimelineData.items}></Timeline>
-            </div> */}
-            <div className="col-12 col-md-7 offset-md-5">
-                <button className='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded' onClick={() => { setPage(page + 1) }} >Load more</button>
+
+            <Timeline items={data}></Timeline>
+            <div className="text-center">
+                {
+                    lastPage ? null : <button className='bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded' onClick={() => { setPage(page + 1) }} >Load more</button>
+                }
+                
             </div>
             <Footer></Footer>
         </>
