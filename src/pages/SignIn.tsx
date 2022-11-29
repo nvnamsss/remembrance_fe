641473ba95f53206678a800102febba0c12230b3
nvnamsss <nvnam.c@gmail.com>
2022-11-30 00:30:21 +0700

    Updated signin

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -5,6 +5,7 @@ import { doLogin, useAuthDispatch, useAuthState } from '../contexts/Auth';
 import { AuthenticationData } from '../dtos/Auth';
 import { AuthRequest, signin } from '../services/Auth';
 import jwt from 'jwt-decode' // import dependency
+import { Button, FloatingLabel, Form } from 'react-bootstrap';
 
 function SignIn(): ReactElement {
     const dispatch = useAuthDispatch();
@@ -19,7 +20,7 @@ function SignIn(): ReactElement {
 
     useEffect(() => {
         if (auth["user"]) {
-            navigate(from, {replace: false});
+            navigate(from, { replace: false });
         }
     }, [])
 
@@ -29,11 +30,12 @@ function SignIn(): ReactElement {
 
     useEffect(() => {
         if (auth["user"]) {
-            navigate(from, {replace: false});
+            navigate(from, { replace: false });
         }
     }, [submitted])
 
     const handleSubmit = async (e: any) => {
+        console.log('hi mom');
         e.preventDefault();
         let req: AuthRequest = {
             username: username,
@@ -45,7 +47,7 @@ function SignIn(): ReactElement {
         }
 
         console.log("res", res);
-        
+
         doLogin(dispatch, username, res.data.access_token, res.data.refresh_token, res.data.profile);
         setSubmitted(submitted + 1);
         return;
@@ -86,17 +88,32 @@ function SignIn(): ReactElement {
 
     return (
         <>
-            <div className='application-main'>
+            <div className='form-outline mx-auto' style={{ width: 400 }}>
                 <div className='auth-form px-3'>
                     <div className='auth-form-header'>
                         <h1 className='text-align-center'>Sign in to Remembrance</h1>
                     </div>
-                    <div className='auth-form-body mt-3'>
-                        <form acceptCharset='UTF-8' action='/sign-up' method='post' onSubmit={handleSubmit}>
-                            <input type='hidden'></input>
+                    <div className='auth-form-body'>
+
+                        <Form acceptCharset='UTF-8' action='/sign-up' method='post' onSubmit={handleSubmit}>
+                            <FloatingLabel className="mb-2" controlId="floatingName" label="Username">
+                                <Form.Control type="text" style={{height: 48}} placeholder="Username" value={username} onChange={(newValue) => { setUser(newValue.target.value); }} />
+                            </FloatingLabel>
+
+                            <FloatingLabel className="mb-2" controlId="floatingContent" label="Password">
+                                <Form.Control type="password" style={{height: 48}} placeholder="Password" value={pwd} onChange={(newValue) => { setPwd(newValue.target.value); }} />
+                            </FloatingLabel>
+
+                            <div className='col text-center'>
+                                <Button className="btn btn-primary btn-block" variant='primary' type='submit'>
+                                    Login
+                                </Button>
+                            </div>
+
+                            {/* <input type='hidden'></input>
                             <label>Username</label>
                             <input
-                                type='text' name='login' className='form-control input-block js-login-field'
+                                type='text' name='login' className='form-control'
                                 onChange={(e) => setUser(e.target.value)}
                                 autoComplete="off"
                                 value={username}
@@ -106,7 +123,7 @@ function SignIn(): ReactElement {
                             <div className='position-relative'>
                                 <label>Password</label>
                                 <input
-                                    type='password' name='password' className='form-control input-block js-login-field'
+                                    type='password' name='password' className='form-control'
                                     onChange={(e) => setPwd(e.target.value)}
                                     value={pwd}
                                     required>
@@ -115,8 +132,8 @@ function SignIn(): ReactElement {
                                 <input
                                     type='submit' name='commit' value='Sign in' className='btn btn-primary btn-block js-sign-in-button'
                                 ></input>
-                            </div>
-                        </form>
+                            </div> */}
+                        </Form>
                     </div>
                 </div>
             </div>
